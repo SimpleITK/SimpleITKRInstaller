e271909b5ef3a8fb36f7229e5f2696937a04591f
Richard Beare <richard.beare@monash.edu>
2016-08-21 19:43:53 +1000

    make j and removing lua stuff

--- circleci_R_commands.R
+++ circleci_R_commands.R
@@ -4,6 +4,7 @@
 ## devtools install
 install.packages("devtools", lib="~/Rlibs", repo="http://cloud.r-project.org/")
 
+makej <- as.numeric(Sys.getenv("MAKE_J"))
 
 # set up the configure vars 
 configvars <- paste0('--configure-vars="MAKEJ=', makej, ' RTESTON=ON USEDISTCC=1"')
