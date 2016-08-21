0e6a563f6e92afa3e27f6337e34e7640bb98204a
Richard Beare <richard.beare@monash.edu>
2016-08-21 17:49:25 +1000

    included parts of lua version and moved R commands to separate script, added options to configure script to enable testing

---
+++ circleci_R_commands.R
@@ -0,0 +1,13 @@
+## Placing the R commands in a script as it gets too messy
+## to do it all on the command line
+
+## devtools install
+install.packages("devtools", lib="Rlibs", repo="http://cloud.r-project.org/")
+
+## Get the build environment variables
+
+makej <- as.numeric(Sys.getenv("MAKE_J"))
+
+# set up the configure vars 
+configvars <- paste0('--configure-vars="MAKEJ=', makej, ' RTESTON=ON USEDISTCC=1"')
+devtools::install("/home/ubuntu/SimpleITKRInstaller", args=configvars)
